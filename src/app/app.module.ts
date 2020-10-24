672d32192795d98ddc576929519945b6399d4e06
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-10-24 17:04:23 +0200

    angular material staff, next commits i will delete angular material. i dont need for this example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //materials
+import {MatGridListModule} from '@angular/material/grid-list';
+
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { LoginComponent } from './login/login.component';
@@ -24,6 +27,7 @@ import {MatInputModule} from '@angular/material/input';
     LoginComponent
   ],
   imports: [
+    MatGridListModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
