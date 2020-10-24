e7eb43a2c9146b7f87626f7845b3af48797c4127
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-10-24 17:04:39 +0200

    register component logic

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  title = 'Registro';
+  user= {
+    name:'',
+    email:'',
+    password:'',
+    repeat_password:''
+  };
+  submited = false;
+
+  constructor(
+
+  ) { }
 
   ngOnInit(): void {
+
+  }
+
+
+  register(){
+    // alert('Usuario registrado');
+    console.log('Click !');
+    console.log(this.user);
+    this.submited = true;
+    if (this.user.password === this.user.repeat_password) {
+      alert('Submit!');
+    }
+    else{
+      alert('Error: Las contraseñas no coinciden !');
+    }
+    this.submited = false;
   }
 
 }
