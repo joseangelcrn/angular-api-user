1ba9ad5b44e4f0f6095df57db520f2c11360a4c2
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-10-26 13:56:25 +0100

    Login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HerokuService } from '../heroku.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  title = 'Login';
+  user= {
+    email:'',
+    password:''
+  };
+  submited = false;
+  message = null;
+
+
+  constructor(
+    private herokuService:HerokuService
+  ) { }
 
   ngOnInit(): void {
   }
+
+  login(){
+      this.submited = true;
+      this.message = null;
+
+      console.log('user');
+      console.log(this.user);
+
+      this.herokuService.login(this.user)
+          .subscribe(
+            data => {
+              // this.message ='Usuario creado correctamente';
+              console.log('response');
+              // console.log(data);
+              let token = data['access_token'];
+              console.log('token');
+              console.log(token);
+
+              if (token != null) {
+                localStorage.setItem('token', JSON.stringify(token));
+              }
+            },
+            error =>{
+              // this.message ='Ha ocurrido un error al registrar el usuario';
+              console.log('error');
+              console.log(error);
+            })
+            .add(
+              ()=>{
+                this.submited = false;
+              }
+            );
+    }
 
 }
