d7c87b6f6d7c11f1e54af7f7cfddf84f6d8ff753
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-10-26 13:53:35 +0100

    login method

--- src/app/heroku.service.ts
+++ src/app/heroku.service.ts
@@ -8,11 +8,18 @@ export class HerokuService {
 
   private REST_API_SERVER = " https://api-auth-user.herokuapp.com/api";
   private SIGNUP = "/signup";
+  private LOGIN = "/login";
+  private HOME = "/user";
+  private LOGOUT = "/logout";
 
 
   constructor(private http: HttpClient) {}
 
   public register(user){
     return this.http.post(this.REST_API_SERVER+this.SIGNUP, user);
+  }
+
+  public login(user) {
+    return this.http.post(this.REST_API_SERVER+this.LOGIN, user);
   }
 }
